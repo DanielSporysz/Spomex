b5b288399975013c85e4aa13af7ed76720f8f57f
DanielSporysz <daniel_spory@o2.pl>
2020-04-22 21:16:04 +0200

    bootstrapped spring project

---
+++ src/main/java/pl/spomex/bazodan/SpomexAPI.java
@@ -0,0 +1,11 @@
+package pl.spomex.bazodan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpomexAPI {
+    public static void main(String[] args) {
+        SpringApplication.run(SpomexAPI.class, args);
+    }
+}
